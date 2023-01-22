4c750d7d844579af259ca00b0c0931e50152901d
Candace Samuels <121878380+CSAMU4@users.noreply.github.com>
2023-01-21 23:54:02 -0500

    Add files via upload

---
+++ arrayInt.java
@@ -0,0 +1,36 @@
+package assignment;
+
+public class arrayInt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// create array with the values 3,9, 23, 64, 2, 8, 28, 93. 
+		
+		int [] ages = {3, 9, 23, 64, 2, 8, 28, 93};
+			System.out.println("first element:" + ages[0]); 
+			System.out.println("last element:" + ages[ages.length-1]);
+		
+		// add a new age to the array and repeat 
+		int[] newAges = {3, 9, 23, 64, 2, 8, 28, 93, 29}; 
+			System.out.println("new last element:" + newAges[newAges.length-1]); 
+			
+		// use a loop to iterate through the array 
+		for (int i = 0; i < ages.length; i++); 
+			System.out.println(ages.length);
+			
+		
+
+			
+			
+			
+			
+			
+			
+			
+		
+		
+		
+		
+	}
+
+}
