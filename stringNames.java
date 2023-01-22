0001203546f7580579c05e41f7ae1b0550e7c268
Candace Samuels <121878380+CSAMU4@users.noreply.github.com>
2023-01-21 23:55:45 -0500

    Add files via upload

---
+++ stringNames.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package assignment;
+
+/**
+ * @author Candace Samuels
+ *
+ */
+public class stringNames {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//create an array that contains names: Sam, Tommy, Tim, Sally, Buck, Bob 
+		String[] names = {"Sam", "Tommy", "Tim,", "Sally", "Buck", "Bob",}; 
+			System.out.println(names);
+			
+		// calculate sum of all letters in new array 
+		
+		int letterSum = 0; 
+		
+		for (int count = 0; count < names.length; count++); 
+			letterSum += names[letterSum].length();
+				System.out.println("Letter sum =" + letterSum + ".");
+				
+				
+		
+			
+	
+	
+			
+	}
+
+}
